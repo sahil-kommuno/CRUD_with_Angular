c4d54f913aee5c429cd7f5f37c0a1b9846342672
sahil-kommuno <sahil.kommuno@gmail.com>
2025-06-29 17:18:07 -0400

    first commit

--- Crud-angular-Backend/Routes/User.formdata.route.ts
+++ Crud-angular-Backend/Routes/User.formdata.route.ts
@@ -3,6 +3,8 @@ const userFormData = require('../APIs/User.formdata');
 const router = expresss.Router();
 import upload from '../Image.Upload/user.Upload.Image';
 import loggerDetail from '../Middleware/commonMiddleware/common.Middleware';
+import { adminlogin,postadmindata } from '../APIs/adminlogger';
+const {jwtAuthMiddleware} =require("../Middleware/commonMiddleware/Authentication.JWT")
 
 router.post(
   '/postdata',
@@ -10,13 +12,15 @@ router.post(
   loggerDetail,
   userFormData.postdata
 );
-router.get('/getdata', loggerDetail, userFormData.getdata);
-router.delete('/deletedata/:id', loggerDetail, userFormData.deletedata);
+router.get('/getdata', loggerDetail,jwtAuthMiddleware, userFormData.getdata);
+router.delete('/deletedata/:id', loggerDetail,jwtAuthMiddleware, userFormData.deletedata);
 router.put(
   '/updatedata/:id',
   upload.single('image'),
-  loggerDetail,
+  loggerDetail,jwtAuthMiddleware,
   userFormData.updatedata
 );
+router.post("/adminlogin",loggerDetail,adminlogin)
+router.post("/postadmindata",loggerDetail,postadmindata)
 
 module.exports = router;
